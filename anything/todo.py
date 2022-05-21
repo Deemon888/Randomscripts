8eb69ab5b75df11a8310830667684fc615579ecc
Deemon888 <98923031+Deemon888@users.noreply.github.com>
2022-05-21 01:03:19 -0600

    fixed

--- anything/todo.py
+++ anything/todo.py
@@ -6,7 +6,7 @@ os.system('clear')
 
 todolist = []
 print('to-do list: ' + str(todolist))
-
+time.sleep(1)
 listItem = input("What do you want to add to your list: ")
 
 while True:
@@ -15,12 +15,12 @@ while True:
     elif listItem != "":
         todolist.append(listItem)
         break
-print(listItem + 'added to list')
+print(listItem + ' added to list')
 print('hashing list to file')
-
+time.sleep(1)
 #hashing
-todolist_hashed = hl.md5(bytes(todolist, 'utf-8')).hexdigest()
-with open(todolisthashed.txt, 'wb') as tdlh:
+todolist_hashed = hl.md5(bytes(listItem, 'utf-8')).hexdigest()
+with open("todolisthashed.txt", 'w') as tdlh:
 	tdlh.write(todolist_hashed)
 
 print('done')
