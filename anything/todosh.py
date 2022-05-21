c81e60920277c729ba339ec57c9c3fa3302af27a
Deemon888 <98923031+Deemon888@users.noreply.github.com>
2022-05-21 02:53:15 -0600

    oof

---
+++ anything/todosh.py
@@ -0,0 +1,18 @@
+import os
+import time
+import hashlib as hl
+
+todolist = []
+
+def help():
+  print("""some commands are\n
+  addtolist
+  hashlist
+  addlisttofile
+  addhashedlisttofile
+  hashmyinput
+  say
+  dir?""")
+
+def addtolist():
+# im tired
