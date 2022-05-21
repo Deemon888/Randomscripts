879119f8bc2605adbb885e060279ae35602625ed
Deemon888 <98923031+Deemon888@users.noreply.github.com>
2022-05-21 00:17:04 -0600

    Create todo.py
    hell ya

---
+++ anything/todo.py
@@ -0,0 +1,8 @@
+import os
+import hashlib as hl
+import time
+
+os.system('clear')
+
+todolist = ['shut my mouth', 'read a book', 'play geomatry dash', 'go to bed']
+print('to-do list: ' + str(todolist))
