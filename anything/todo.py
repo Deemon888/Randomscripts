fb046c5ccee9dc5cdb8d3b27a5b6189d7d923b0b
Deemon888 <98923031+Deemon888@users.noreply.github.com>
2022-05-21 00:52:40 -0600

    Update todo.py
    done

--- anything/todo.py
+++ anything/todo.py
@@ -9,9 +9,21 @@ print('to-do list: ' + str(todolist))
 
 listItem = input("What do you want to add to your list: ")
 
-if listItem == "":
-    listItem = input("What do you want to add to your list: ")
-elif listItem != "":
-    todolist.append(listItem)  
+while True:
+    if listItem == "":
+        listItem = input("What do you want to add to your list: ")
+    elif listItem != "":
+        todolist.append(listItem)
+        break
+print(listItem + 'added to list')
+print('hashing list to file')
 
-# we can use functions too
+#hashing
+todolist_hashed = hl.md5(bytes(todolist, 'utf-8')).hexdigest()
+with open(todolisthashed.txt, 'wb') as tdlh:
+	tdlh.write(todolist_hashed)
+
+print('done')
+#hashing#
+
+# dont think this will work bc its a list ans not a sring but will try...
