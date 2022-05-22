212500b327317a3470ed1d055e13e833897f2fa1
Deemon888 <98923031+Deemon888@users.noreply.github.com>
2022-05-22 00:35:27 -0600

    added more

--- cacu./htmlVer/cacuSjsVer.js
+++ cacu./htmlVer/cacuSjsVer.js
@@ -1,12 +1,15 @@
 let h1 = document.getElementById('heading1')
 let number1 = document.getElementById('no1')
 let number2 = document.getElementById('no2')
+let b1 = document.getElementById('button1')
 // main main
 
 let optA = document.getElementById('add')
 let optS = document.getElementById('sub')
 let optM = document.getElementById('mul')
 let optD = document.getElementById('div')
+//
+let opc = [optA, optS, optM, optD]
 // ^^^ main vars
 
 let x = parseInt(number1)
@@ -33,3 +36,18 @@ function checkeq(opt) {
 		mtap = True
 	}
 }
+// functions
+
+b1.addEventListen('keypress', function event() {
+	checkeq(opc)
+	if (madd === True) {
+		h1.innerText = x + ' + ' + y + ' = ' add
+	} else if (msub === True) {
+		h1.innerText = x + ' - ' + y + ' = ' sub
+	} else if (mdiv === True) {
+		h1.innerText = x + ' / ' + y + ' = ' div
+	} else if (mmul === True) {
+		h1.innerText = x + ' x ' + y + ' = ' mul
+	}
+})
+// button
