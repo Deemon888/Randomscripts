dc9f05b50b4921e63d975bb806496702245c07a4
Deemon888 <98923031+Deemon888@users.noreply.github.com>
2022-05-22 01:41:16 -0600

    .

--- cacu./htmlVer/cacuSjsVer.js
+++ cacu./htmlVer/cacuSjsVer.js
@@ -1,1 +1,6 @@
+let h1 = document.getElementById('heading1')
+let no1 = document.getElementById('no1')
+let no2 = document.getElementById('no2')
+// main vars
 
+let b1 = document.getElementById('')
