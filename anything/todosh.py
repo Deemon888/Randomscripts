6f3cdae81feb60f525e9ac9aafaa869061e585b2
Deemon888 <98923031+Deemon888@users.noreply.github.com>
2022-05-21 19:17:27 -0600

    made function

--- anything/todosh.py
+++ anything/todosh.py
@@ -1,13 +1,18 @@
 import os
 import time
 import hashlib as hl
+import sys
 
+user = os.system('echo "$USER"')
+sys.tracebacklimit = -1
 todolist = []
 
 def help():
-  print("""some commands are\n
+  print("""some commands are:\n
+  todolist
   addtolist
   hashlist
+  resetlist
   addlisttofile
   addhashedlisttofile
   hashmyinput
@@ -15,4 +20,58 @@ def help():
   dir?""")
 
 def addtolist():
-# im tired
+	if prompt[1] == '[]':
+		toadd = input('> ')
+		todolist.append(toadd)
+	else:
+		quickadd = prompt[len(prompt) - 1]
+		if IndexError:
+			print('error')
+			continue
+			pass
+		todolist.append(quickadd)
+		
+def todolist():
+	print(str(todolist))
+	
+def resetlist():
+	todolist = []
+	
+def hashlist():
+	strtodolist = str(todolist)
+	if strtodolist == '[]':
+		print('[error]: nothing to hash')
+	else:
+		todolist_hashed = hl.md5(bytes(strtodolist, 'utf-8')).hexdigest()
+	print(todolist_hashed + ' - hashed')
+	
+def addlisttofile():
+	with open('todolist.txt', 'w') as todolistfile:
+		todolistfile.write(str(todolist))
+	
+def addhashedlisttofile():
+	strtodolist = str(todolist)
+	todolist_hashed = hl.md5(bytes(strtodolist, 'utf-8')).hexdigest()
+	with open('todolisthashed.txt', 'w') as todolisthashedfile:
+		todolisthashedfile.write(todolist_hashed)
+		
+def hashmyinput():
+	input1 = input('> ')
+	input1hashed = hl.md5(bytes(input1, 'utf-8')).hexdigest()
+	print(input1hashed)
+	
+def say():
+	if prompt[1] == '':
+		tosay = input('> ')
+		print(tosay)
+	else:
+		qtosay = prompt[len(prompt) - 1]
+		if IndexError:
+			print('error')
+			continue
+			pass
+		print(qtosay)
+		
+def dir?():
+	dir = os.system('pwd')
+	print(dir)
