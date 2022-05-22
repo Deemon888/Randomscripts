6795fec7f95f1ca7c40fd3e7de32068b63a57686
Deemon888 <98923031+Deemon888@users.noreply.github.com>
2022-05-21 23:02:40 -0600

    vars

--- cacu./htmlVer/cacuSjsVer.js
+++ cacu./htmlVer/cacuSjsVer.js
@@ -1,1 +1,15 @@
 let h1 = document.getElementById('heading1')
+let number1 = document.getElementById('no1')
+let number2 = document.getElementById('no2')
+// main vars
+
+let x = parseInt(number1)
+let y = parseInt(number2)
+// main2 vars
+
+var add = (x + y)
+var sub = (x - y)
+var div = (x / y)
+var mul = (x * y)
+var tap = (x ** y)
+// extra vars
