cd9de0c8ccf427da56b20197017d382b6677d6a3
Deemon888 <98923031+Deemon888@users.noreply.github.com>
2022-05-23 18:46:54 -0600

    i hope this works

--- cacu./htmlVer/cacuSjsVer.js
+++ cacu./htmlVer/cacuSjsVer.js
@@ -7,3 +7,18 @@ let b1 = document.getElementById('button1')
 const opts = document.querySelector('#operations')
 // imp vars
 
+let x = parseInt(no1)
+let y = parseInt(no2)
+// ex vars
+
+b1.addEvenetListener('click', e => {
+	if (opts.value === 'add') {
+		h1.innerHTML = x + ' + ' + y + ' = ' + (x + y)
+	} else if (opts.value === 'sub') {
+		h1.innerHTML = x + ' - ' + y + ' = ' + (x - y)
+	} else if (opts.value === 'mul') {
+		h1.innerHTML = x + ' x ' + y + ' = ' + (x * y)
+	} else if (opts.value === 'div') {
+		h1.innerHTML = x + ' / ' + y + ' = ' + (x / y)
+	}
+});
